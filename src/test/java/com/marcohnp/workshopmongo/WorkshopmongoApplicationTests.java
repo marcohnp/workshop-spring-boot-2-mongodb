33f4911c4725543688fe4c2e1deaa886023be9a5
Marco Henrique Nunes Pereira <marcohnp@hotmail.com>
2020-05-26 11:16:43 -0300

    Created project

---
+++ src/test/java/com/marcohnp/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcohnp.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
